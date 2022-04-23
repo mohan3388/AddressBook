678391d27c0ca9cd08f5aeea5be2e55fb2910792
mohan3388 <68227271+mohan3388@users.noreply.github.com>
2022-04-23 17:55:22 +0530

    Update Contact.cs

--- AddressBook/Contact.cs
+++ AddressBook/Contact.cs
@@ -1,26 +1,22 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Address_Book
+namespace AddressBook
 {
     public class Contact
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
         public string State { get; set; }
-        public int Zip { get; set; }
-        public long Phone { get; set; }
         public string Email { get; set; }
-
-        internal static void Remove(Contact delete)
-        {
-            throw new NotImplementedException();
-        }
+        public int Zip { get; set; }
+        
+        public long Phone { get; set; }
+       
     }
-
 }
