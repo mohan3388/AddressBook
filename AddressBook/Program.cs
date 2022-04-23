3ca79ad49917f861d79e7af3d620a94a321890ab
mohan3388 <68227271+mohan3388@users.noreply.github.com>
2022-04-23 13:31:01 +0530

    Update Program.cs

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,22 +1,21 @@
-﻿using System;
-using AddressBookSolution;
-class program
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
 {
-    public static void Main(String[] args)
+    public class AddressBook
     {
-        Console.WriteLine("Enter the Address Details");
 
-        AddressBook addressBook = new AddressBook();
-        addressBook.Display();
 
-    Contact address=new Contact ();
-        address.FirstName =Console.ReadLine();
-        address.LastName =Console.ReadLine();
-        address.Address=Console.ReadLine();
-        address.State = Console.ReadLine();
-        address.Email = Console.ReadLine();
-        address.Zip =Convert.ToInt32(Console.ReadLine());
-        address.Mobile =Convert.ToInt64 (Console.ReadLine());
-        Console.WriteLine(address.FirstName+" "+ address.LastName+" "+ address.Address+" "+ address.State+" "+ address.Email+" "+ address.Zip+" "+ address.Mobile);
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Enter Contact Details: ");
+
+          Address add = new Address();
+            add.Display();
+        }
     }
 }
