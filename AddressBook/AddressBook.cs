1113c741c12ad4a4ac9485c0750d286ce02b5404
mohan3388 <68227271+mohan3388@users.noreply.github.com>
2022-04-23 13:30:29 +0530

    Update AddressBook.cs

--- AddressBook/AddressBook.cs
+++ AddressBook/AddressBook.cs
@@ -1,63 +1,62 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Collections.Generic;
-using System;
 
-namespace AddressBookSolution
+namespace AddressBook
 {
-    public class AddressBook
+    public class Address
     {
-        List<Contact> addressBook = new List<Contact>();
-        public AddressBook()
+        List<Contact> addBook = new List<Contact>();    
+
+        public Address()
         {
-            Contact address1 = new Contact()
+            Contact contact1 = new Contact()
             {
-                FirstName = Console.ReadLine(),
-                LastName = Console.ReadLine(),
-                Address = Console.ReadLine(),
-                State = Console.ReadLine(),
-                Email = Console.ReadLine(),
-                Zip = Convert.ToInt32(Console.ReadLine()),
-                Mobile = Convert.ToInt64(Console.ReadLine())
+               firstName = "mohan",
+               lastName = "sahu",
+               Address = "green valey",
+               City = "bhilai",
+               State = "cg",
+               Email = "sahu@12gmail.com",
+               Zip = 12451,
+               Phone = 744551552
+                    
             };
-            Contact address2 = new Contact()
+            Contact contact2 = new Contact()
             {
-                FirstName = "mohan",
-                LastName = "sahu",
-                Address = "green valley",
-                State = "bhilai",
-                Email = "sahu@12gmail",
-                Zip = 452014,
-                Mobile = 789852487,
+                firstName = "rajesh",
+               lastName = "sah",
+               Address = "green valey",
+               City = "bhilai",
+               State = "cg",
+               Email = "sahu@12gmail.com",
+               Zip = 12451,
+               Phone = 744551552
             };
-            Contact address3 = new Contact()
+            Contact contact3 = new Contact()
             {
-                FirstName = "rajesh",
-                LastName = "saha",
-                Address = "green valley",
-                State = "bhilai",
-                Email = "dsas@gmail",
-                Zip = 452014,
-                Mobile = 789852487,
+                firstName = Console.ReadLine(),
+               lastName = Console.ReadLine(),
+               Address = Console.ReadLine(),
+               City = Console.ReadLine(),
+               State = Console.ReadLine(),
+               Email = Console.ReadLine(),
+               Zip = Convert.ToInt32(Console.ReadLine()),
+               Phone = Convert.ToInt32(Console.ReadLine()),
             };
-            addressBook.Add(address1);
-            addressBook.Add(address2);
-            addressBook.Add(address3);
-        }
-        public void AddContactToAddressBook(Contact contact)
-        {
-            addressBook.Add(contact);
+            addBook.Add(contact1);
+            addBook.Add(contact2);
+            addBook.Add(contact3);
         }
         public void Display()
         {
-            foreach (var contact in addressBook)
+            foreach(var contact in addBook)
             {
-                Console.WriteLine(contact.FirstName + " " + contact.LastName + " " + contact.Address + " " + contact.State + " " + contact.Email + " " + " " + contact.Zip + " " + contact.Mobile);
+                Console.WriteLine(contact.firstName + " " + contact.lastName + " " + contact.Address + " " + contact.City + " " + contact.State + " " + contact.Email + " " + contact.Zip + " " + contact.Phone);
             }
         }
-
+        
     }
 }
