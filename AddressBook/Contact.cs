d8ba08dbf6629448728edda5e422d767902e3bea
mohan3388 <68227271+mohan3388@users.noreply.github.com>
2022-04-23 13:31:55 +0530

    Update Contact.cs

--- AddressBook/Contact.cs
+++ AddressBook/Contact.cs
@@ -1,22 +1,22 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AddressBookSolution
+namespace AddressBook
 {
     public class Contact
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
         public string Address { get; set; }
+        public string City { get; set; }
         public string State { get; set; }
         public string Email { get; set; }
         public int Zip { get; set; }
-        public long Mobile { get; set; }
-
-
-
+        
+        public long Phone { get; set; }
+       
     }
 }
